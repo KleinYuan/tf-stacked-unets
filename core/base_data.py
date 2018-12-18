f2114037a27db167e2fb79472b4f7169a81492ec
KleinYuan <kaiwen.yuan1992@gmail.com>
2018-12-17 22:02:26 -0800

    add imagenet data loader

--- core/base_data.py
+++ core/base_data.py
@@ -1,4 +1,7 @@
-import numpy as np
+import cv2
+import os
+import tarfile
+import random
 
 
 class Model(object):
@@ -15,10 +18,10 @@ class Model(object):
 
     def _load_data(self):
         _data_name = self.config.name
-        _num_data = self.config.number
         self.logger.info("Loading {}...".format(_data_name))
         if _data_name == 'mscoco':
             from tensor2tensor.data_generators import mscoco as _data_sets
+            _num_data = self.config.number
             self.data_sets = _data_sets.mscoco_generator(
                 data_dir='data/',
                 tmp_dir='tmp/',
@@ -29,10 +32,55 @@ class Model(object):
         elif _data_name == 'cifar10':
             import tensorflow as tf
             (self.x_train, self.y_train), (self.x_val, self.y_val) = tf.keras.datasets.cifar10.load_data()
-            # self.y_train = np.eye(10)[self.y_train[:, ]]
-            # self.y_train = np.reshape(self.y_train, (self.y_train.shape[0], self.y_train.shape[-1]))
-            # self.y_val = np.eye(10)[self.y_val[:, ]]
-            # self.y_val = np.reshape(self.y_val, (self.y_val.shape[0], self.y_val.shape[-1]))
+        elif _data_name == 'ILSVRC2012':
+            _img_dir = self.config.img_dir
+            _label_fp = self.config.label_fp
+            _label_dict = {}
+            with open(_label_fp) as f:
+                labels_content = f.readlines()
+            for _cls in labels_content:
+                _cls_split = _cls.split(' ')
+                _label_dict[_cls_split[0]] = {
+                    'idx': _cls_split[1],
+                    'name': _cls_split[2]
+                }
+
+            print("Extracting data from .tar ......")
+            _img_extracted_dir_ls = []
+            for (dirpath, dirnames, filenames) in os.walk(_img_dir):
+                for filename in filenames:
+                    if filename.endswith('.tar'):
+                        WNID = filename.split('.')[0]
+                        print("  Extracting {} .tar ......".format(WNID))
+                        _abs_dir = os.sep.join([dirpath, filename])
+                        _extracted_to = os.sep.join([dirpath, WNID]) + '/'
+                        _tar = tarfile.open(_abs_dir)
+                        _tar.extractall(path=_extracted_to)
+                        _tar.close()
+                        _img_extracted_dir_ls.append(_extracted_to)
+            print("Constructing training data sets .....")
+            x_data = []
+            y_data = []
+            for _img_extracted_dir in _img_extracted_dir_ls:
+                for (dirpath, dirnames, filenames) in os.walk(_img_extracted_dir):
+                    for filename in filenames:
+                        if filename.endswith('.JPEG'):
+                            WNID = filename.split('.')[0]
+                            _abs_dir = os.sep.join([dirpath, filename])
+                            _x = cv2.imread(_abs_dir)
+                            _x = cv2.resize(_x, dsize=(224, 224, 3))
+                            x_data.append(_x)
+                            y_data.append(int(_label_dict[WNID]['idx']))
+
+            shuffle_ls = list(zip(x_data, y_data))
+            random.shuffle(shuffle_ls)
+            x_data, y_data = zip(*shuffle_ls)
+            split_idx = int(self.config.split_ratio * len(x_data))
+            self.x_train = x_data[:split_idx]
+            self.y_train = y_data[:split_idx]
+            self.x_val = x_data[split_idx:]
+            self.y_val = y_data[split_idx:]
+
 
     @staticmethod
     def batch_iterator(xs, ys, batch_size=1):
